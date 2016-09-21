aef8b80c46f27422d023f45c07bd12063fa085d3
Kei Nohguchi <kei@nohguchi.com>
2016-09-21 16:05:30 -0700

    account.go: For account related methods

---
+++ account.go
@@ -0,0 +1,11 @@
+// account related methods
+package main
+
+import (
+	"github.com/digitalocean/godo"
+)
+
+func (c* Client) account() (account *godo.Account, err error) {
+	account, _, err = c.do.Account.Get()
+	return
+}
--- client.go
+++ client.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"golang.org/x/oauth2"
 
@@ -12,7 +11,7 @@ import (
 
 type Client struct{
 	token string
-	c     *godo.Client
+	do    *godo.Client
 }
 
 func (c *Client) Token() (*oauth2.Token, error) {
@@ -23,18 +22,18 @@ func (c *Client) Token() (*oauth2.Token, error) {
 func (c *Client) open(token string) error {
 	c.token = token
 	oauthClient := oauth2.NewClient(oauth2.NoContext, c)
-	c.c = godo.NewClient(oauthClient)
+	c.do = godo.NewClient(oauthClient)
 	return nil
 }
 
 func (c *Client) handle(cmd string) error {
 	switch cmd {
 	case "account":
-		account, _, err := c.c.Account.Get()
+		obj, err := c.account()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-		fmt.Printf("%s: %v\n", cmd, account)
+		fmt.Printf("%s: %v\n", cmd, obj)
 		return nil
 	default:
 		return fmt.Errorf("%q: not supported", cmd)
