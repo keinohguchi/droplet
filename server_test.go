a461bd744d32f504a6933e2f1eb264b06c564f9d
Kei Nohguchi <kei@nohguchi.com>
2016-09-26 22:02:00 -0700

    server_test.go: Simplest test ever! :)

---
+++ server_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHandle(t *testing.T) {
+	s := &Server{}
+	tests := []struct {
+		req  *request
+		want *reply
+	}{
+		{
+			req: &request{cmd: "BogusCmd"},
+			want: &reply{
+				dataType: invalid,
+				data:     nil,
+				err:      fmt.Errorf("BogusCmd not supported\n"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		go s.handle(tt.req)
+		got := <-replies
+		if got.err != tt.want.err &&
+			fmt.Sprint(got.err) != fmt.Sprint(tt.want.err) {
+			t.Errorf("s.handle() sends %q, want %q\n",
+				got.err, tt.want.err)
+		}
+	}
+}
